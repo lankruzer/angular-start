3b93174bf5774edc0e2fd48737e349432eca2723
Andrei <andrei.raichonok@gmail.com>
2019-10-22 22:49:53 +0300

    add interface for courListItem

--- src/app/pages/page-course-list/course-list-item/course-list-item.component.ts
+++ src/app/pages/page-course-list/course-list-item/course-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {CourseListItem} from "../course-list-item.model";
 
 @Component({
   selector: 'app-course-list-item',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-list-item.component.scss']
 })
 export class CourseListItemComponent implements OnInit {
+  @Input() public courseItem: CourseListItem;
+
+  public formatDuration = (duration: number) : string => {
+    const hours = Math.floor(duration / 60);
+    const hoursStr = hours > 0 ? hours + 'h' : '';
+    const minutes = duration % 60;
+    const minutesStr = minutes > 0 ? minutes + 'min' : '';
+    return hoursStr + ' ' + minutesStr;
+  };
 
   constructor() { }
 
--- src/app/pages/page-course-list/course-list/course-list.component.ts
+++ src/app/pages/page-course-list/course-list/course-list.component.ts
@@ -1,21 +1,29 @@
 import {Component, OnInit} from '@angular/core';
+import {CourseListItem} from "../course-list-item.model";
 
 @Component({
   selector: 'app-course-list',
   templateUrl: './course-list.component.html',
   styleUrls: ['./course-list.component.scss']
 })
+
 export class CourseListComponent implements OnInit {
-  public courseItemList = [
+  public courseItemList: CourseListItem[] = [
     {
-      id: '0',
+      id: 0,
       title: 'First course',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid blanditiis consequatur cum dignissimos eius excepturi laborum minus, modi neque, quas repellendus repudiandae sequi vero. Ab dicta dolorem perspiciatis placeat similique.'
+      creationDate: '6 Nov, 2018',
+      duration: 120,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid blanditiis consequatur cum dignissimos' +
+        ' eius excepturi laborum minus, modi neque, quas repellendus repudiandae sequi vero. Ab dicta dolorem perspiciatis placeat similique.'
     },
     {
-      id: '1',
+      id: 1,
       title: 'Second course',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid blanditiis consequatur cum dignissimos eius excepturi laborum minus, modi neque, quas repellendus repudiandae sequi vero. Ab dicta dolorem perspiciatis placeat similique.'
+      creationDate: '12 Nov, 2019',
+      duration: 80,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid blanditiis consequatur cum dignissimos' +
+        ' eius excepturi laborum minus, modi neque, quas repellendus repudiandae sequi vero. Ab dicta dolorem perspiciatis placeat similique.'
     }
   ];
 
