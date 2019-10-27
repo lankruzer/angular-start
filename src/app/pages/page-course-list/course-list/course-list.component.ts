44892a061267090d7d92c7bfc25b7ddc9b794aad
Andrei <andrei.raichonok@gmail.com>
2019-10-28 00:47:33 +0300

    move course list init to ngOnInit

--- src/app/pages/page-course-list/course-list/course-list.component.ts
+++ src/app/pages/page-course-list/course-list/course-list.component.ts
@@ -8,24 +8,7 @@ import { CourseListItem } from "../course-list-item.model";
 })
 
 export class CourseListComponent implements OnInit {
-  public courseItemList: CourseListItem[] = [
-    {
-      id: 0,
-      title: 'First course',
-      creationDate: '6 Nov, 2018',
-      duration: 120,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid blanditiis consequatur cum dignissimos' +
-        ' eius excepturi laborum minus, modi neque, quas repellendus repudiandae sequi vero. Ab dicta dolorem perspiciatis placeat similique.'
-    },
-    {
-      id: 1,
-      title: 'Second course',
-      creationDate: '12 Nov, 2019',
-      duration: 80,
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid blanditiis consequatur cum dignissimos' +
-        ' eius excepturi laborum minus, modi neque, quas repellendus repudiandae sequi vero. Ab dicta dolorem perspiciatis placeat similique.'
-    }
-  ];
+  public courseItemList: CourseListItem[];
 
   public onLoadMore = () : void => {
     console.log('Load more');
@@ -35,6 +18,24 @@ export class CourseListComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.courseItemList = [
+      {
+        id: 0,
+        title: 'First course',
+        creationDate: '6 Nov, 2018',
+        duration: 120,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid blanditiis consequatur cum dignissimos' +
+          ' eius excepturi laborum minus, modi neque, quas repellendus repudiandae sequi vero. Ab dicta dolorem perspiciatis placeat similique.'
+      },
+      {
+        id: 1,
+        title: 'Second course',
+        creationDate: '12 Nov, 2019',
+        duration: 80,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid blanditiis consequatur cum dignissimos' +
+          ' eius excepturi laborum minus, modi neque, quas repellendus repudiandae sequi vero. Ab dicta dolorem perspiciatis placeat similique.'
+      }
+    ];
   }
 
 }
